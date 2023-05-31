e0735e6f8d5d4d80eabb10b5d3e0c2f15a32fbc5
Lucyfer <liene.krista@gmail.com>
2023-05-31 04:09:24 +0300

    Initial commit

---
+++ Controller/NewsTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NewsTableViewCell.swift
+//  AppleNewsFeed
+//
+//  Created by liene.krista.neimane 30/05/2023.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var newsTitleLabel: UILabel!
+    @IBOutlet weak var newsImageView: UIImageView!
+    
+}
